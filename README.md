3671c776cd55135460de68e5d36546934fb572cc
jaycenhorton <jaycen.horton@gmail.com>
2019-03-13 16:31:12 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# WIP - DO NOT REFERENCE
+
+# Install everything
+
+With yarn
+
+`yarn install`
+
+or with NPM
+
+`npm install`
+
+# Start the slides
+
+With yarn
+
+`yarn start`
+
+or with NPM
+
+`npm run install`
---
+++ theme.js
@@ -0,0 +1,10 @@
+export default {
+  // add a custom font
+  font: '"ITC Avant Garde Gothic Std Bold", "Helvetica", "Arial", sans-serif',
+  // custom colors
+  colors: {
+    text: '#FFFFFF',
+    background: '#265F69',
+    link: '#0ff',
+  },
+};
